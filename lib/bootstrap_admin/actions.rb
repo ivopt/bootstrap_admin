af95f92f0f5edb7667a5a331318efad825b2157e
Ivo Jesus <ivo.jesus@gmail.com>
2013-04-13 11:10:13 +0100

    BugFix: Added "new" and "update" action methods 'cause instance variables weren't being created!

--- lib/bootstrap_admin/actions.rb
+++ lib/bootstrap_admin/actions.rb
@@ -10,50 +10,61 @@ module BootstrapAdmin
   # that declares the usage of bootstrap_admin
   module Actions
 
-    # The actual action methods!
+    # The actual controller action methods!
     module InstanceMethods
-      # Default index action, uses the controllers responder
-      def index
-        namespaced_response collection
-      end
 
-      # Default show action, uses the controllers responder
-      def show
-        namespaced_response instance
-      end
+      # =============================================================================
+      # Lists (all) items
+      def index; namespaced_response collection; end
 
-      # Creates a new record
+      # =============================================================================
+      # Shows a specific item
+      def show; namespaced_response instance; end
+
+      # =============================================================================
+      # Displays the form to create a new item
+      def new; namespaced_response instance; end
+
+      # =============================================================================
+      # Creates a new item
       def create
         instance model_class.create(params[model_name])
         instance.save
         namespaced_response instance
       end
 
-      # Updates the current record
+      # =============================================================================
+      # Displays the form to edit an existing item
+      def edit; namespaced_response instance; end
+
+      # =============================================================================
+      # Updates the existing item
       def update
         instance.update_attributes params[model_name]
         namespaced_response instance
       end
 
-      # Destroys the current record
+      # =============================================================================
+      # Destroys an existing item
       def destroy
         instance.destroy
         namespaced_response instance
       end
-      # -----------------------------------------------------------------------------
-      # -----------------------------------------------------------------------------
+
       protected
-        # -----------------------------------------------------------------------------
+        # =============================================================================
         # @return [String] model name based on the controller's name
         def model_name
           collection_name.singularize
         end
-        # -----------------------------------------------------------------------------
+
+        # =============================================================================
         # @return [ActiveRecord::Base] model class based on the controller's name
         def model_class
           model_name.classify.constantize
         end
-        # -----------------------------------------------------------------------------
+
+        # =============================================================================
         # @return [ActiveRecord::Base] instance
         def instance instance_var = nil
           if instance_var
@@ -66,7 +77,8 @@ module BootstrapAdmin
             ivar
           end
         end
-        # -----------------------------------------------------------------------------
+
+        # =============================================================================
         # @return [ActiveRecord::Relation] collection of all items for the model
         # named after the controller
         def collection collection_var = nil
@@ -80,14 +92,15 @@ module BootstrapAdmin
             cvar
           end
         end
-        # -----------------------------------------------------------------------------
+
+        # =============================================================================
         # @return [String] collection name based on the controller name
         def collection_name
           self.class.name.sub("Controller", "").underscore.split('/').last
         end
-      # -----------------------------------------------------------------------------
+
       private
-        # -----------------------------------------------------------------------------
+        # =============================================================================
         # Prepares a response using the controllers reponder.
         def namespaced_response var
           # namespace is a property of BootstrapAdmin::Config and gets called because
@@ -95,14 +108,14 @@ module BootstrapAdmin
           response_obj = [self.namespace].flatten.compact << var
           respond_with *response_obj
         end
-      # -----------------------------------------------------------------------------
+
     end # module InstanceMethods
-    # -----------------------------------------------------------------------------
-    ###############################################################################
+
+    # =============================================================================
+    # includes InstanceMethods on the target class
     def self.included base
-      # base.extend ClassMethods
       base.send :include, InstanceMethods
     end
-    ###############################################################################
+
   end # module Actions
 end # module BootstrapAdmin
