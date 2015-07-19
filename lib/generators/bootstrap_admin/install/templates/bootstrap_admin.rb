d5eb14229f997263c9312271ad321bea21439439
LynxEyes <ivo.jesus@gmail.com>
2015-07-19 01:16:42 +0100

    initializer entry to explain how to override default ignored fields

--- lib/generators/bootstrap_admin/install/templates/bootstrap_admin.rb
+++ lib/generators/bootstrap_admin/install/templates/bootstrap_admin.rb
@@ -8,11 +8,20 @@ BootstrapAdmin.setup do |config|
   # ==> Paginator configuration
   # Configure the number of results shown per page by the paginator.
   # Default value: 10
+  #
   # config.paginator_page_size = 10
 
   # ==> UI Styles
   # Configure the css class names that each action wrapper will have
   # Default value: {index: %w(table-bordered table-striped)}
+  #
   # config.ui_styles[:index] << "my_awesome_style_class"
+
+  # ==> Default ignored fields
+  # Define the fields that the automatic view generator should ignore
+  # when building the index view and the new/edit forms.
+  # Default value: %i(id created_at updated_at)
+  #
+  # config.default_ignored_fields += %i(some other fields)
 
 end
